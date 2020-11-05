179d2697708e4488cbdabc9d0a668d3173f69da9
Cole Williamson <williamsoncole@gmail.com>
2020-11-05 14:54:01 -0600

    Weighted sales for upcoming months added.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import AssessmentIcon from '@material-ui/icons/Assessment';
-import { makeStyles, Card } from '@material-ui/core';
+import { makeStyles, Card, Typography } from '@material-ui/core';
 import axios from 'axios';
+import { format } from 'date-fns';
 import PageHeader from './PageHeader';
 import Header from './Header';
 import SideMenu from './SideMenu';
-import { format } from 'date-fns';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -20,24 +20,44 @@ const useStyles = makeStyles(() => ({
   cardGroupOne: {
     display: 'flex',
     justifyContent: 'space-around',
+    width: '100%',
+    // backgroundColor: 'red',
   },
-  cardGroudTwo: {
+  cardGroupTwo: {
     display: 'flex',
     justifyContent: 'space-around',
+    width: '100%',
+    // backgroundColor: 'blue',
   },
   card: {
     display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
-    width: '30%',
+    width: '100%',
     height: '300px',
-    margin: '10px',
+    margin: '35px',
+    // backgroundColor: 'green',
+  },
+  weightedCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  weightedContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
   },
 }));
 
 const Home = () => {
   const [invoiceTotal, setInvoiceTotal] = useState(0);
   const [invoiceTotalThisMonth, setInvoiceTotalThisMonth] = useState(0);
+  const [weightedSales, setWeightedSales] = useState([]);
+  const [months, setMonths] = useState([]);
   const classes = useStyles();
 
   useEffect(() => {
@@ -56,15 +76,32 @@ const Home = () => {
   //   });
   // }, []);
 
-  // useEffect(() => {
-  //   axios({
-  //     method: 'GET',
-  //     url: 'http://localhost:4000/sales/weighted',
-  //   }).then((response) => {
-  //     const sales = response.data;
-  //     console.log(sales);
-  //   });
-  // }, []);
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:4000/sales/weighted',
+    }).then((response) => {
+      console.log(response.data);
+      setWeightedSales(response.data[0]);
+      setMonths(response.data[1]);
+    });
+  }, []);
+
+  const weightedSalesNumbers = weightedSales.map((amount) => {
+    if (amount === null) {
+      return null;
+    }
+    // return <h2 className={classes.weightedContent}>{amount}</h2>;
+    return <Typography variant="h5">{amount}</Typography>;
+  });
+
+  const weightedMonths = months.map((month) => {
+    if (month === null) {
+      return null;
+    }
+    // return <h2 className={classes.weightedContent}>{month.Month}</h2>;
+    return <Typography variant="h5">{month.Month}</Typography>;
+  });
 
   return (
     <div className={classes.root}>
@@ -78,22 +115,19 @@ const Home = () => {
       <div className={classes.cards}>
         <div className={classes.cardGroupOne}>
           <Card className={classes.card}>
-            <h1>Total Invoices: {invoiceTotal}</h1>
-          </Card>
-          <Card className={classes.card}>
-            <h1>Invoice total from this month: {invoiceTotalThisMonth}</h1>
-            <h3></h3>
-          </Card>
-          <Card className={classes.card}>
-            <h1>Invoices from this month: {invoiceTotalThisMonth}</h1>
-            <h3></h3>
+            <div className={classes.weightedCard}>
+              <Typography variant="h4">
+                Weigthed Sales for Upcoming Months
+              </Typography>
+
+              <div className={classes.weightedContent}>
+                {weightedSalesNumbers}
+              </div>
+              <div className={classes.weightedContent}>{weightedMonths}</div>
+            </div>
           </Card>
         </div>
-        <div className={classes.cardGroudTwo}>
-          <Card className={classes.card}>
-            <h1>Invoices from this month: {invoiceTotalThisMonth}</h1>
-            <h3></h3>
-          </Card>
+        <div className={classes.cardGroupTwo}>
           <Card className={classes.card}>
             <h1>Invoices from this month: {invoiceTotalThisMonth}</h1>
             <h3></h3>
