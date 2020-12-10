abd9664295f6196b7a7352dcf4b6971e3161b822
Cole Williamson <williamsoncole@gmail.com>
2020-12-10 12:15:10 -0600

    Added administrator password field to signup.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,35 +20,32 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignContent: 'center',
-    height: '100px',
-    width: '500px',
-    padding: '25px',
-    /* margin: 100px 50px, */
   },
 
-  quickbooksButton: {
-    backgroundImage: `url(${Image})`,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textDecoration: 'none',
-    // color: 'white',
-    // backgroundColor: 'green',
-    backgroundSize: '100%',
-    marginBottom: '10px',
-    width: '300px',
-    height: '52px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    '&:hover': { backgroundImage: `url(${hoverImage})` },
-  },
+  // quickbooksButton: {
+  //   backgroundImage: `url(${Image})`,
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   textDecoration: 'none',
+  //   // color: 'white',
+  //   // backgroundColor: 'green',
+  //   backgroundSize: '100%',
+  //   marginBottom: '10px',
+  //   width: '300px',
+  //   height: '52px',
+  //   border: 'none',
+  //   borderRadius: '5px',
+  //   cursor: 'pointer',
+  //   '&:hover': { backgroundImage: `url(${hoverImage})` },
+  // },
   card: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     padding: theme.spacing(6),
-    margin: '100px',
+    margin: '150px',
+    width: '400px',
   },
   formArea: {
     display: 'flex',
@@ -58,14 +55,13 @@ const useStyles = makeStyles((theme) => ({
   emailAndPassword: {
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-around',
-    // width: '80%',
+    width: '300px',
   },
 }));
 
 function Login() {
   const classes = useStyles();
-  const [signup, setSignup] = useState(true);
+  const [signup, setSignup] = useState(false);
 
   const loginHandler = () => {
     axios.get();
@@ -82,13 +78,12 @@ function Login() {
   const SignupSchema = Yup.object().shape({
     firstName: Yup.string()
       .required('Required')
-      .min(2, 'First Name is Too Short'),
-    lastName: Yup.string()
-      .required('Required')
-      .min(2, 'Last Name is Too Short'),
-    isSalesPerson: Yup.boolean().required('Required'),
+      .min(2, 'Not Enough Characters'),
+    lastName: Yup.string().required('Required').min(2, 'Not Enough Characters'),
+    isSalesperson: Yup.boolean().typeError('Required').required('Required'),
     email: Yup.string().email('Invalid email').required('Required'),
     password: Yup.string().required('Required').min(8, 'Password is Too Short'),
+    administratorPassword: Yup.string().required('Required'),
     // passwordConfirmation: Yup.string().oneOf(
     //   [Yup.ref('password'), null],
     //   'Passwords must match'
@@ -107,93 +102,122 @@ function Login() {
         title="Login"
         subtitle="Log in to access site features."
       />
-      <Card className={classes.card}>
-        {signup ? (
-          <div className={classes.formArea}>
-            <Formik
-              initialValues={{
-                firstName: '',
-                lastName: '',
-                isSalesPerson: null,
-                email: '',
-                password: '',
-                // passwordConfirmation: '',
-              }}
-              validationSchema={SignupSchema}
-              onSubmit={(values) => {
-                signupHandler(values);
-              }}
-            >
-              {({ values, errors, touched }) => (
-                <Form>
-                  <div className={classes.emailAndPassword}>
-                    <Typography>Sign Up</Typography>
-                    <Field as={TextField} label="First Name" name="firstName" />
-                    <ErrorMessage name="firstName" />
-                    <Field as={TextField} label="Last Name" name="lastName" />
-                    <ErrorMessage name="lastName" />
-                    <Field
-                      as={TextField}
-                      select
-                      label="Are you a Salesperson"
-                      name="isSalesPerson"
-                    >
-                      <MenuItem value={true}>Yes</MenuItem>
-                      <MenuItem value={false}>No</MenuItem>
-                    </Field>
-                    <ErrorMessage name="isSalesPerson" />
-                    <Field as={TextField} label="Email" name="email" />
-                    <ErrorMessage name="email" />
-                    <Field as={TextField} label="Password" name="password" />
-                    <ErrorMessage name="password" />
-                    {/* <Field as={TextField} name="passwordConfirmation" /> */}
-                    <Button type="submit">Submit</Button>
-                    <Typography onClick={() => setSignup(!signup)}>
-                      Have an account? Click here to sign in.
-                    </Typography>
-                    <pre>{JSON.stringify(values, null, 4)}</pre>
-                    <pre>{JSON.stringify(errors, null, 4)}</pre>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        ) : null}
-        {!signup ? (
-          <div className={classes.formArea}>
-            <Formik
-              initialValues={{ email: '', password: '', password2: '' }}
-              validationSchema={LoginSchema}
-              onSubmit={(values) => {
-                console.log(values);
-              }}
-            >
-              {({ errors, touched }) => (
-                <Form>
-                  <div className={classes.emailAndPassword}>
-                    <Typography>Sign In</Typography>
-                    <Field as={TextField} label="Email" name="email" />
-                    <ErrorMessage name="email" />
-                    <Field as={TextField} label="Password" name="password" />
-                    <ErrorMessage name="password" />
-                    <Button type="submit">Submit</Button>
-                    <Typography onClick={() => setSignup(!signup)}>
-                      Need to create an account? Click here to sign up.
-                    </Typography>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        ) : null}
+      <div className={classes.login}>
+        <Card className={classes.card}>
+          {signup ? (
+            <div className={classes.formArea}>
+              <Formik
+                initialValues={{
+                  firstName: '',
+                  lastName: '',
+                  isSalesperson: null,
+                  email: '',
+                  password: '',
+                  administratorPassword: '',
+                  // passwordConfirmation: '',
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={(values) => {
+                  signupHandler(values);
+                }}
+              >
+                {({ values, errors, touched }) => (
+                  <Form>
+                    <div className={classes.emailAndPassword}>
+                      <Typography>Sign Up</Typography>
+                      <Field
+                        as={TextField}
+                        label="First Name"
+                        name="firstName"
+                      />
+                      <ErrorMessage name="firstName" />
+                      <Field as={TextField} label="Last Name" name="lastName" />
+                      <ErrorMessage name="lastName" />
+                      <Field
+                        as={TextField}
+                        select
+                        label="Are you a Salesperson"
+                        name="isSalesperson"
+                      >
+                        <MenuItem value={true}>Yes</MenuItem>
+                        <MenuItem value={false}>No</MenuItem>
+                      </Field>
+                      <ErrorMessage name="isSalesperson" />
+                      <Field as={TextField} label="Email" name="email" />
+                      <ErrorMessage name="email" />
+                      <Field
+                        as={TextField}
+                        type="password"
+                        label="Password"
+                        name="password"
+                      />
+                      <ErrorMessage name="password" />
+                      <Field
+                        as={TextField}
+                        type="password"
+                        label="Administrator Password"
+                        name="administratorPassword"
+                      />
+                      <ErrorMessage name="administratorPassword" />
+                      {/* <Field as={TextField} name="passwordConfirmation" /> */}
+                      <Button type="submit">Submit</Button>
+                      <Typography onClick={() => setSignup(!signup)}>
+                        Have an account? Click here to sign in.
+                      </Typography>
+                      {/* <pre>{JSON.stringify(values, null, 4)}</pre>
+                    <pre>{JSON.stringify(errors, null, 4)}</pre> */}
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          ) : null}
+          {!signup ? (
+            <div className={classes.formArea}>
+              <Formik
+                initialValues={{ email: '', password: '', password2: '' }}
+                validationSchema={LoginSchema}
+                onSubmit={(values) => {
+                  console.log(values);
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className={classes.emailAndPassword}>
+                      <Typography>Sign In</Typography>
+                      <Field
+                        as={TextField}
+                        type="email"
+                        label="Email"
+                        name="email"
+                      />
+                      <ErrorMessage name="email" />
+                      <Field
+                        as={TextField}
+                        type="password"
+                        label="Password"
+                        name="password"
+                      />
+                      <ErrorMessage name="password" />
+                      <Button type="submit">Submit</Button>
+                      <Typography onClick={() => setSignup(!signup)}>
+                        Need an account? Click here to sign up.
+                      </Typography>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          ) : null}
 
-        {/* <a
+          {/* <a
           className={classes.quickbooksButton}
           href="http://localhost:4000/oauth"
         >
           quickbooks Button
         </a> */}
-      </Card>
+        </Card>
+      </div>
     </div>
   );
 }
